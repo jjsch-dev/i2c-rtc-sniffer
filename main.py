99c84417ce9fb9af51a346a26f5176e8ad43367e
jjsch-dev <juanjoseschiavoni@hotmail.com>
2021-06-03 21:43:09 -0300

    add -o parameter and bin for win32

--- main.py
+++ main.py
@@ -12,24 +12,28 @@
 
 """
 import argparse
+import json
 
 parser = argparse.ArgumentParser(description="analyzes the I2C bus sniffer output of an RTC M41T81")
-parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
-parser.add_argument("-f", "--filename", required=True, help="filename, in text format.")
+parser.add_argument('--version', action='version', version='%(prog)s 0.1.1')
+parser.add_argument("-f", "--filename", required=True, help="input filename, in text format.")
+parser.add_argument("-o", "--output", required=False, help="output filename, in json format (i2c_rtc.json).")
 
 args = parser.parse_args()
 
+json_data = []
 
 def parse_record(record):
-    # Use a breakpoint in the code line below to debug your script.
-    # setting the maxsplit parameter to 1, will return a list with 2 elements!
+    global json_data
 
     if len(record) == 39:
         x = record.split("a")
         if x[0] == "sD0" and x[2] == "sD1":
-            print("{{year:{}, month:{}, day:{}, hour:{}, "
-                  "minute:{}, seconds:{}, tenths:{}, status:\'ok\'}}".format(x[10], x[9], x[8],
+            json_data.append("{{year:{}, month:{}, day:{}, hour:{}, "
+                          "minute:{}, seconds:{}, tenths:{}, status:\'ok\'}}".format(x[10], x[9], x[8],
                                                                              x[6], x[5], x[4], x[3]))
+            print("{}:{}:{}:{} {}/{}/{} ok".format(x[6], x[5], x[4], x[3],
+                                                     x[8], x[9], x[10]))
         else:
             print("{data:{}, status:\'Error\'}".format(record))
     elif record != "sD0a0CasD1a31np":
@@ -51,3 +55,10 @@ if __name__ == '__main__':
 
     except FileNotFoundError:
         print("error file not found")
+
+    try:
+        if args.output is not None:
+            with open(args.output, 'w') as outfile:
+                json.dump(json_data, outfile, indent=4)
+    except FileNotFoundError:
+        print("error json file not found")
