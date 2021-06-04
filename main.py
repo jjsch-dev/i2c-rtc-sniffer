069839f54ef75de9b4cfa218dae0ca865f7b9984
jjsch-dev <juanjoseschiavoni@hotmail.com>
2021-06-04 20:49:20 -0300

    add -p (port) to access the sniffer by serial / USB port

--- main.py
+++ main.py
@@ -13,52 +13,90 @@
 """
 import argparse
 import json
+import serial
 
 parser = argparse.ArgumentParser(description="analyzes the I2C bus sniffer output of an RTC M41T81")
-parser.add_argument('--version', action='version', version='%(prog)s 0.1.1')
-parser.add_argument("-f", "--filename", required=True, help="input filename, in text format.")
+parser.add_argument('--version', action='version', version='%(prog)s 0.1.3')
+parser.add_argument("-f", "--filename", required=False, help="input filename, in text format.")
 parser.add_argument("-o", "--output", required=False, help="output filename, in json format (i2c_rtc.json).")
+parser.add_argument("-p", "--port", required=False, help="serial com port (win = COMxxx, linux = ttyXXXX)")
+parser.add_argument("-b", "--baudrate", help="serial transfer rate (def = 230400)", default="230400")
 
 args = parser.parse_args()
 
 json_data = []
 
-def parse_record(record):
+# Analyze the records sent by the tracker finished in LF CR.
+# Returns False when there are no valid characters.
+def parse_record(line):
     global json_data
+    line = line.strip()  # remove leading/trailing white spaces
 
-    if len(record) == 39:
-        x = record.split("a")
-        if x[0] == "sD0" and x[2] == "sD1":
+    # The date and time of the RTC is obtained with the following procedure:
+    # First write to the I2C address of the device (D0), the address of the RTC register pointer to zero.
+    # And then it reads with D1 (1 indicates reading) the nine 8-bit registers that contain the date and
+    # time, and in its 9 bits it sends ack, and ends with nak.
+    if len(line) == 39:
+        x = line.split("a")
+        if len(x) == 12 and x[0] == "sD0" and x[2] == "sD1":
             json_data.append("{{year:{}, month:{}, day:{}, hour:{}, "
-                          "minute:{}, seconds:{}, tenths:{}, status:\'ok\'}}".format(x[10], x[9], x[8],
-                                                                             x[6], x[5], x[4], x[3]))
+                             "minute:{}, seconds:{}, tenths:{}, status:\'ok\'}}".format(x[10], x[9], x[8],
+                                                                                        x[6], x[5], x[4], x[3]))
             print("{}:{}:{}:{} {}/{}/{} ok".format(x[6], x[5], x[4], x[3],
-                                                     x[8], x[9], x[10]))
+                                                   x[8], x[9], x[10]))
         else:
-            print("{data:{}, status:\'Error\'}".format(record))
-    elif record != "sD0a0CasD1a31np":
-        print("{data:{}, status:\'Error\'}".format(record))
+            json_data.append("{{data:{}, status:\'nak\'}}".format(line))
+            print("{} nak".format(line))
+    elif line != "sD0a0CasD1a31np" and len(line) > 0:
+        print("{}, unk".format(line))
+        json_data.append("{{data:{}, status:\'unk\'}}".format(line))
+    else:
+        return False
+    return True
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
 
+def parse_file(filename):
     try:
-        with open(args.filename) as f:
+        with open(filename) as f:
             lines = f.readlines()  # list containing lines of file
 
             for line in lines:
-                line = line.strip()  # remove leading/trailing white spaces
-#                columns = [item.strip() for item in line.split(' ')]
-#                dt = columns[1].split('/')
-#                tm = columns[2].split(':')
                 parse_record(line)
-
     except FileNotFoundError:
         print("error file not found")
 
+
+def parse_serial_com(port, baudrate):
+    device = None
     try:
-        if args.output is not None:
-            with open(args.output, 'w') as outfile:
+        device = serial.Serial(port, baudrate, 8, 'N', 1, timeout=1)
+    except:
+        print("error can't open the serial port: {}".format(port))
+
+    if device is not None:
+        while True:
+            try:
+                line = device.readline()
+                if line:
+                    if parse_record(line.decode('utf-8')):
+                        save_json(args.output)
+            except serial.SerialException as e:
+                print("error reading serial port")
+
+
+def save_json(file_name):
+    try:
+        if file_name is not None:
+            with open(file_name, 'w') as outfile:
                 json.dump(json_data, outfile, indent=4)
     except FileNotFoundError:
         print("error json file not found")
+
+if __name__ == '__main__':
+
+    if args.port is not None:
+        parse_serial_com(args.port, args.baudrate)
+    elif args.filename is not None:
+        parse_file(args.filename)
+
+    save_json(args.output)
